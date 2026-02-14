59b96a90cd9e32c59268b185b6d79ae2d321d6af
Orderion <ioorder677@gmail.com>
2026-02-14 16:04:32 +0000

    Create Shop.jsx

---
+++ Beme-Frontend/src/pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductGrid from "../components/ProductGrid";
+
+const Shop = () => {
+  return (
+    <div className="shop-page">
+      <h2>New Arrivals</h2>
+      <ProductGrid />
+    </div>
+  );
+};
+
+export default Shop;
