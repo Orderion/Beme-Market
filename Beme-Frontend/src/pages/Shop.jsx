a9bc01613ccfe7c27627748d3f7ae0bf446027b3
Superstackedgiveaways <stackedgiveaway225@gmail.com>
2026-02-16 14:41:16 +0000

    Full staged commit

--- Beme-Frontend/src/pages/Shop.jsx
+++ Beme-Frontend/src/pages/Shop.jsx
@@ -5,7 +5,7 @@ import "./Shop.css";
 const Shop = () => {
   return (
     <div className="shop-page">
-      <h2>New Arrivals</h2>
+      <h2>New In Stock</h2>
       <ProductGrid />
     </div>
   );
