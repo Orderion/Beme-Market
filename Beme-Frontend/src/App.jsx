4d6b2fcfa2e1072c5a2e73de1bb135d9225a256a
Superstackedgiveaways <stackedgiveaway225@gmail.com>
2026-02-23 15:28:35 +0000

    div fix

--- Beme-Frontend/src/App.jsx
+++ Beme-Frontend/src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
           style={{
             position: "fixed",
             top: "35px",
-            right: "40px",
+            right: "55px",
             width: "60px",
             height: "30px",
             background: darkMode ? "#222" : "#e5e5e5",
