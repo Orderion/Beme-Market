01d8a2af97e47e39d2793dce297afb32e2658c23
Superstackedgiveaways <stackedgiveaway225@gmail.com>
2026-02-23 15:39:43 +0000

    div fix

--- Beme-Frontend/src/App.jsx
+++ Beme-Frontend/src/App.jsx
@@ -42,7 +42,7 @@ export default function App() {
           onClick={() => setDarkMode(!darkMode)}
           style={{
             position: "fixed",
-            top: "150px",
+            top: "160px",
             right: "15px",
             width: "60px",
             height: "30px",
