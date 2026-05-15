4fbbb14f054ec6a9fbbc701534ec9d6f85ef4ccf
Superstackedgiveaways <stackedgiveaway225@gmail.com>
2026-05-15 13:34:09 +0000

    auth changes

--- Beme-Frontend/src/pages/Signup.jsx
+++ Beme-Frontend/src/pages/Signup.jsx
@@ -221,13 +221,9 @@ export default function Signup() {
 
         <Link to="/" className="auth-logo">
           <div className="auth-logo-mark">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white"
-              strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-              <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/>
-              <line x1="3" y1="6" x2="21" y2="6"/>
-              <path d="M16 10a4 4 0 0 1-8 0"/>
-            </svg>
-          </div>
+           <img src="/Favicon-white.PNG" alt="" width="22" height="22"
+            style={{ objectFit: "contain" }} />
+           </div>
           <span className="auth-logo-name">Beme Market</span>
         </Link>
 
