365df772e8d01e2433612fa964cad45b856ad003
Superstackedgiveaways <stackedgiveaway225@gmail.com>
2026-05-26 12:03:41 +0000

    Fix port binding

--- Beme-Backend/src/server.js
+++ Beme-Backend/src/server.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
